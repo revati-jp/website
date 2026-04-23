4fc25b9fea8bc55d527e265b3517258e9e1d26f0
Rinrin.rs <rinrin0413.rs@gmail.com>
2026-04-23 22:27:51 +0900

    📚 Docs: add reminder to keep `HEADER_ITEMS` in sync with `SECTIONS`

--- src/lib/scripts/data/SECTIONS.ts
+++ src/lib/scripts/data/SECTIONS.ts
@@ -1,3 +1,5 @@
+// If update the `SECTIONS` JS constant,
+// also adjust and update the `HEADER_ITEMS` JS constant in `/src/lib/scripts/data/HEADER_ITEMS.ts`.
 export const SECTIONS = [
 	'about',
 	'news',
